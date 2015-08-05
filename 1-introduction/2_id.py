9f895c1b061d30ca91ddc24b8658219931c6b374
Krishnaprasad Varma <krshnaprsad@gmail.com>
2015-08-05 15:30:46 +0530

    Checking in Files

--- 1-introduction/2_id.py
+++ 1-introduction/2_id.py
@@ -1,4 +1,8 @@
 name = "Ram"
 age = 51
+
+print name
+print age
+
 print id(name)
 print id(age)
--- 2-variables/4_string_methods.py
+++ 2-variables/4_string_methods.py
@@ -9,8 +9,16 @@ s.endswith("country")
 s.find("my")
 # => 9
 
+s.find("xx")
+# => -1
+
 s.index("country")
 # => 12
+
+s.index("xx")
+# Traceback (most recent call last):
+#   File "<stdin>", line 1, in <module>
+# ValueError: substring not found
 
 # s.index("hello")
 # => Traceback (most recent call last):
